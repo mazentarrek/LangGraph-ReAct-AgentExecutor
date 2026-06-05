720f1d4e2e18dd91f5ae060973ba9347bfbeb575
Mazen Tarek <mazen.hanafy.ext@siemens.com>
2026-06-05 18:25:44 +0300

    implementing graph and main

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.messages import HumanMessage
+from langgraph.graph import MessagesState, StateGraph, END
+from regex import E
+
+from nodes import run_agent_reasoning, tool_node
 
 load_dotenv()
 
+AGENT_REASON="agent_reason"
+ACT="act"
+LAST=-1
+
+def should_continue(state:MessagesState) -> str:
+    if not state["messages"][LAST].tool_calls:
+        return END
+    return ACT
+
+flow = StateGraph(MessagesState)
+
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+# Whatever coes from the should_continue function we want to act it to the end node or act node
+flow.add_conditional_edges(AGENT_REASON, should_continue, {
+    END:END,
+    ACT:ACT
+})
+
+flow.add_edge(ACT, AGENT_REASON)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
 def main():
     print("Hello from langgraph-agentexecutor!")
+    res = app.invoke({"messages": [HumanMessage(content="What is the temperature in tokyo? List it and triple it")]})
+    print(res["messages"][LAST].content)
 
 
 if __name__ == "__main__":
